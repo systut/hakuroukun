0267526163b219f473a5af646751dabe780fd35b
ttran01 <ttran01@qibitech.com>
2023-03-19 23:00:14 +0900

    :+1: update hakuroukun control node

---
+++ src/hakuroukun_control/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup
+
+setup(
+    name="hakuroukun_control",
+    version="1.0.0",
+    description="A package for Hakuroukun",
+    url="https://ise.tut.ac.jp/",
+    author="Tran Viet Thanh",
+    author_email="tran.viet.thanh.du@tut.ac.jp",
+    license="MIT",
+    packages=["src/hakuroukun_control"],
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "License :: OSI Approved :: BSD License",
+        "Operating System :: POSIX :: Linux",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+    ],
+)
---
+++ src/hakuroukun_control/src/hakuroukun_control/hakuroukun_control_node.py
@@ -0,0 +1,76 @@
+#! /usr/bin/env python3
+# -*- coding: utf-8 -*-
+# vim:fenc=utf-8
+#
+# Distributed under terms of the MIT license.
+
+# Standard library
+
+
+# External library
+import serial
+import rospy
+from geometry_msgs.msg import Twist
+
+
+class HakuroukunControlNode(object):
+    """!
+    @brief This class privide a ROS node to control hakuroukun robot using serial
+    connection to motor control circuit
+    """
+    # ==========================================================================
+    # PUBLICH FUNCTION
+    # ==========================================================================
+    def __init__(self) -> None:
+        """! Class constructor
+        """
+        rospy.init_node("hakuroukun_control_node", anonymous=True)
+        
+        port = rosparam.get_param("/port")
+
+        baud_rate = rosparam.get_param("/baud_rate")
+
+        controller_rate = rosparam.get_param("/controller_rate")
+
+        self.connection = serial.Serial(port, baud_rate, timeout=None)
+
+        self.velocity_subscriber = rospy.Subscriber(
+            "/cmd_vel", Twist, self._velocity_callback)
+
+        self.timer = rospy.Timer(
+            rospy.Duration(controller_rate), 
+            self._timer_callback)
+
+    def run(self) -> None:
+        """! Start ros node
+        """
+        rospy.spin()
+
+    # ==========================================================================
+    # PRIVATE FUNCTION
+    # ==========================================================================
+    def _timer_callback(self, event: rospy.TimerEvent) -> None:
+        """! Callback function for velocity timer
+        @param[in] event: timer event
+        """
+        acceleration_command, steering_command = self._apply_indentification()
+
+        self.connection.write(f"{acceleration_command}{steering_command}")
+    
+    def _velocity_callback(self, msg: Twist) -> None:
+        """! Callback function for velocity subscriber
+        @param[in] msg: velocity message in Twist form
+        """
+        self.velocity_msg = msg
+    
+    def _apply_indentification(self) -> Tuple[str, str]:
+        """! Apply system indentification so as to send the right voltage
+        @param[in] msg: velocity message in Twist form
+        """
+        linear_velocity = self.velocity_msg.linear.x
+
+        angular_velocity = self.velocity_msg.angular.z
+
+        # TODO: Add system indentification equation here
+
+        return acceleration_command, steering_command
