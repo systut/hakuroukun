8324e806c77c300b1add848193e120411f88631d
duc-dn-st <duc.dn.st@gmail.com>
2024-10-22 18:01:29 +0900

    :rabbit: refactor hakuroukun_pose

--- src/hakuroukun_localization/hakuroukun_pose/src/hakuroukun_pose/hakuroukun_pose_node.py
+++ src/hakuroukun_localization/hakuroukun_pose/src/hakuroukun_pose/hakuroukun_pose_node.py
@@ -1,5 +1,6 @@
 import math
-import time
+from datetime import datetime
+import pytz
 import os
 
 import rospy
@@ -7,8 +8,10 @@ from sensor_msgs.msg import NavSatFix
 from sensor_msgs.msg import Imu
 from geometry_msgs.msg import PoseStamped
 from std_msgs.msg import Float64
-from tf.transformations import euler_from_quaternion
+# from tf.transformations import euler_from_quaternion
 import geonav_transform.geonav_conversions as gc
+import tf.transformations as tf
+
 
 class HakuroukunPose:
 
@@ -18,115 +21,120 @@ class HakuroukunPose:
 
     def __init__(self):
 
-        self.file_name = os.getcwd() + "/../catkin_ws/src/data/tut_run/" + str(time.time()) + ".csv"
+        self.log = True
 
         rospy.init_node("robot_localization", anonymous=True)
 
-        # Wait until got first (lon, lat, alt) from gps as origin
-        first_gps_mess = rospy.wait_for_message('/fix', NavSatFix, timeout=10)
-        rospy.loginfo("GPS Data Received")
-        # Wait until got first quad from imu
-        first_imu_mess = rospy.wait_for_message('/imu/data_raw', Imu, timeout=20)
-        
-        rospy.loginfo("IMU Data Received")
+        self._register_parameters()
 
-        self.lat0 = first_gps_mess.latitude        # First GPS Data
-        
-        self.lon0 = first_gps_mess.longitude        # First GPS Data
+        self._register_log_file()
 
-        self.dt = 0.01
-        
-        self.orientation = 90.0
+        self._get_initial_pose()
+
+        self._get_initial_orientation()
 
         rospy.sleep(1)
 
-        ## Pub - Sub
+        self._register_publishers()
 
-        self.gps_sub = rospy.Subscriber("/fix", NavSatFix, self._gps_callback) 
+        self._register_subscribers()
 
-        self.imu_sub = rospy.Subscriber("/imu/data_raw", Imu, self._imu_callback)
-
-        self.pose_pub = rospy.Publisher("/hakuroukun_pose/pose", PoseStamped, queue_size=10)
-
-        self.front_pose_pub = rospy.Publisher("/hakuroukun_pose/front_wheel", PoseStamped, queue_size=10)
-
-        self.orientation_pub = rospy.Publisher("/hakuroukun_pose/orientation", Float64, queue_size=1)
-        
         rospy.sleep(1)
 
-        rospy.Timer(rospy.Duration(0.01), self._publish_pose)
+        self._register_timers()
 
-        rospy.Timer(rospy.Duration(0.01), self._publish_orientation)
-
-        rospy.Timer(rospy.Duration(0.01), self._log_pose)
-        
     def run(self):
         """! Start ros node
         """
-
         rospy.spin()
 
     # ==========================================================================
     # PRIVATE FUNCTION
     # ==========================================================================
+    def _register_parameters(self):
+        """! Register parameters
+        """
+        self.publish_rate = 0.1
 
-    def _get_xy_from_lat_lon(self, lat,lon):
+        self.orientation = 0
 
-        x,y = gc.ll2xy(lat, lon, self.lat0, self.lon0)
+    def _register_subscribers(self):
+        """! Register subscribers
+        """
+        self.gps_sub = rospy.Subscriber(
+            "/fix", NavSatFix, self._gps_callback)
 
-        rotation_angle = math.radians(-13.5255 - 6.3102 + 11.9329 + 90)
+        self.imu_sub = rospy.Subscriber(
+            "/imu/data_raw", Imu, self._imu_callback)
 
-        current_pose_x = x*math.cos(rotation_angle) - y*math.sin(rotation_angle)
+    def _register_publishers(self):
+        """! Register publishers
+        """
+        self.rear_pose_pub = rospy.Publisher(
+            "/hakuroukun_pose/rear_wheel_position", PoseStamped, queue_size=10)
 
-        current_pose_y = x*math.sin(rotation_angle) + y*math.cos(rotation_angle)
+        self.orientation_pub = rospy.Publisher(
+            "/hakuroukun_pose/orientation", Float64, queue_size=1)
 
-        return current_pose_x, current_pose_y
+    def _register_timers(self):
+        """! Register timers
+        """
+        rospy.Timer(rospy.Duration(self.publish_rate),
+                    self._publish_rear_wheel_pose)
 
-    def _get_euler_from_quaternion(self, quad):
-        
-        quaternion = (quad[0], quad[1], quad[2], quad[3])
+        if self.log:
 
-        euler = euler_from_quaternion(quaternion)
+            rospy.Timer(rospy.Duration(self.publish_rate),
+                        self._log_pose)
 
-        return euler
-    
-    def _get_initial_offset(self, yaw):
+    def _register_log_file(self):
+        """! Register log file
+        """
+        current_folder = os.path.dirname(os.path.abspath(__file__))
 
-        if yaw <= math.pi:
+        new_folder = os.path.join(current_folder, '..', '..', '..', 'log_data')
 
-            offset = -yaw
+        new_folder = os.path.normpath(new_folder)
 
-        else:
+        japan_timezone = pytz.timezone('Asia/Tokyo')
 
-            offset = (2 * math.pi - yaw)
+        current_time = datetime.now(japan_timezone).strftime(
+            "position_log_%Y%m%d_%H-%M")
 
-        return offset
-    
-    def _integrate_yaw(self, current_orientation, angular_rate, dt):
+        self.file_name = os.path.join(
+            new_folder, current_time + ".csv")
 
-        current_orientation += (angular_rate+0.36) * dt  # There was an offset with 0.36 deegree when stood still
+    def _get_initial_pose(self):
+        """! Get initial pose
+        """
+        first_gps_mess = rospy.wait_for_message(
+            '/fix', NavSatFix, timeout=10)
 
-        return current_orientation
+        rospy.loginfo("GPS Data Received")
 
-    # ====================================
-    # Callback Functions
-    # ====================================
+        self.initial_lat = first_gps_mess.latitude
 
-    def _gps_callback(self, data):
+        self.initial_lon = first_gps_mess.longitude
 
-        self.current_lat = data.latitude
+    def _get_initial_orientation(self):
+        """! Get initial orientation
+        """
+        rospy.loginfo("IMU Data Received")
 
-        self.current_lon = data.longitude
+    def _gps_callback(self, data: NavSatFix):
 
-        self.current_pose_x, self.current_pose_y = self._get_xy_from_lat_lon(self.current_lat, self.current_lon)
+        self.x_gps, self.y_gps = self._get_xy_from_latlon(
+            data.latitude, data.longitude, self.initial_lat, self.initial_lon)
 
-        l = 0.4
+        gps_to_rear_axis = 0.6
 
-        self.current_front_x = self.current_pose_x + l * math.cos(self.orientation)
+        self.x_rear = self.x_gps - \
+            gps_to_rear_axis * math.cos(self.orientation)
 
-        self.current_front_y = self.current_pose_y + l * math.sin(self.orientation)
+        self.y_rear = self.y_gps - \
+            gps_to_rear_axis * math.sin(self.orientation)
 
-    def _imu_callback(self, data):
+    def _imu_callback(self, data: Imu):
 
         self.quaternion_x = data.orientation.x
         self.quaternion_y = data.orientation.y
@@ -139,61 +147,67 @@ class HakuroukunPose:
         self.linear_acceleration_y = data.linear_acceleration.y
         self.linear_acceleration_z = data.linear_acceleration.z
 
-        self.quad = [self.quaternion_x, self.quaternion_y, self.quaternion_z, self.quaternion_w]
+        self.quad = [self.quaternion_x, self.quaternion_y,
+                     self.quaternion_z, self.quaternion_w]
 
         self.euler = self._get_euler_from_quaternion(self.quad)
 
-        self.yaw = self.euler[2] + 0.139626
+        self.yaw = self.euler[2] + 0.2526352784505572
 
-        if self.yaw < 0:
+    def _publish_rear_wheel_pose(self, timer):
 
-            self.yaw = self.yaw + math.pi * 2
+        rear_wheel_msg = PoseStamped()
+        rear_wheel_msg.header.stamp = rospy.get_rostime()
+        rear_wheel_msg.pose.position.x = self.x_rear
+        rear_wheel_msg.pose.position.y = self.y_rear
+        rear_wheel_msg.pose.position.z = 0.0
+        rear_wheel_msg.pose.orientation.x = self.quaternion_x
+        rear_wheel_msg.pose.orientation.y = self.quaternion_y
+        rear_wheel_msg.pose.orientation.z = self.quaternion_z
+        rear_wheel_msg.pose.orientation.w = self.quaternion_w
 
-        self.orientation = self._integrate_yaw(self.orientation, self.angular_velocity_z, self.dt)
+        rospy.loginfo(
+            f"x: {rear_wheel_msg.pose.position.x}, y: {rear_wheel_msg.pose.position.y}")
+
+        self.rear_pose_pub.publish(rear_wheel_msg)
 
     def _log_pose(self, timer):
 
-        buf = f"{self.current_pose_x}, {self.current_pose_y}, {(self.orientation)}" + "\n"
+        pose = f"{self.x_rear}, {self.y_rear}, {(self.yaw)}" + "\n"
 
-        with open(self.file_name, mode = "a") as f:
+        with open(self.file_name, mode="a") as f:
 
-            f.write(buf)
+            f.write(pose)
 
-    # ====================================
-    # Publish Function
-    # ====================================
-            
-    def _publish_pose(self, timer):
+    def _get_xy_from_latlon(self, latitude, longitude, initial_lat, initial_lon):
 
-        now = rospy.get_rostime()
+        # What is this lol
+        # This is a magic number - the angle between the GPS and the robot's
+        rotation_angle = math.radians(-13.5255 - 6.3102 + 11.9329)
 
-        pose_msg = PoseStamped()
-        pose_msg.header.stamp = now
-        pose_msg.pose.position.x = self.current_pose_x
-        pose_msg.pose.position.y = self.current_pose_y
-        pose_msg.pose.position.z = 0.0
-        pose_msg.pose.orientation.x = self.quaternion_x
-        pose_msg.pose.orientation.y = self.quaternion_y
-        pose_msg.pose.orientation.z = self.quaternion_z
-        pose_msg.pose.orientation.w = self.quaternion_w
+        x_gps, y_gps = gc.ll2xy(latitude, longitude, initial_lat, initial_lon)
 
-        front_pose_msg = PoseStamped()
-        front_pose_msg.header.stamp = now
-        front_pose_msg.pose.position.x = self.current_front_x
-        front_pose_msg.pose.position.y = self.current_front_y
-        front_pose_msg.pose.position.z = 0.0
-        front_pose_msg.pose.orientation.x = self.quaternion_x
-        front_pose_msg.pose.orientation.y = self.quaternion_y
-        front_pose_msg.pose.orientation.z = self.quaternion_z
-        front_pose_msg.pose.orientation.w = self.quaternion_w
+        x_gps_local = x_gps*math.cos(rotation_angle) - \
+            y_gps*math.sin(rotation_angle)
 
-        self.pose_pub.publish(pose_msg)
-        self.front_pose_pub.publish(front_pose_msg)
+        y_gps_local = x_gps*math.sin(rotation_angle) + \
+            y_gps*math.cos(rotation_angle)
 
-    def _publish_orientation(self, timer):
+        return x_gps_local, y_gps_local
 
-        orientation_msg = Float64()
+    def _get_euler_from_quaternion(self, quad):
 
-        orientation_msg.data = float(self.orientation)
+        quaternion = (quad[0], quad[1], quad[2], quad[3])
 
-        self.orientation_pub.publish(orientation_msg)
+        euler = tf.euler_from_quaternion(quaternion)
+
+        return euler
+
+    def _integrate_yaw(self, current_orientation, angular_rate, dt):
+        """! This function calculate the orientation of the robot using the
+            angular rate from the IMU
+        """
+
+        current_orientation += (angular_rate+0.36) * dt
+
+        return current_orientation
